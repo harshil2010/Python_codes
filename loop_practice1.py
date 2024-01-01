3eb85dcad5ac97ae32d2a7aab930ae264f0ac376
harshil2010 <155142412+harshil2010@users.noreply.github.com>
2024-01-01 19:32:34 +0530

    Add files via upload

---
+++ loop_practice1.py
@@ -0,0 +1,82 @@
+'''l=[1,2,3,4,5]
+for i in l :
+     print(i, type(i))'''
+
+#l1=['harshil', 'vihar', 'shyam']
+'''for i in l1:
+    print(i)
+else:
+    print("if  for loop is able to complete it self then only else will execute")
+'''
+'''
+for i in l1:
+    if i == "vihar":
+        break
+    print(i) 
+else :
+    print("execute if for loop will executed completely")
+'''
+'''
+for i in l1:
+    if i == "vihar":
+        continue
+    print(i)
+'''
+'''   
+for i in l1:
+    if i == "vihar":
+        continue
+    print(i)
+else:
+    print("execute if for loop will executed completely")
+'''
+'''range(0,5)
+print(list(range(5)))
+print(list(range(0,5,1)))'''
+'''
+a=list(range(0,20,2))
+print(a)
+
+a=list(range(-10,0))
+print(a)
+'''
+
+#l2 = ['harshil', 'vihar', 'shyam']
+'''print(len(l2))
+print(list(range(len(l2))))
+for i in range(len(l2)):
+    print(l2[i])'''
+
+'''a= list(range(len(l2)-1, -1, -1))
+print(a)
+for i in range(len(l2)-1, -1,-1):
+    print(l2[i])'''
+
+'''l2 = [8,555,625,14,25,46,84,75,45,21,44]
+a= list(range(0, len(l2), 2))
+print(a)
+for i in range(0, len(l2), 2):
+    print(l2[i])'''
+
+'''l = [1,2,3,4,5,6,7,8,9,10]
+res= 0
+for i in l:
+    res = res+i
+    print(res)
+res'''
+
+'''t = (1,2,3,4,5)
+for i in t:
+    print(i)'''
+
+#d = {"name": 'hk', "class": 'ds masters', "topics": '["python", "stats", "ml", "dl"]'}
+#print(d)
+'''for i in d.keys():
+    print(d[i])'''
+
+'''print(d.values())
+for i in d.values():
+    print(i)'''
+
+'''for i in d.items():
+    print(i)'''
